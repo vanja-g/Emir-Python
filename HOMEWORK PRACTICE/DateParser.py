11798e231f1fa662ec943261f3dd1e0bf7a87cd2
emirm <emirmba@gmail.com>
2024-09-07 21:43:02 +0200

    hw

--- HOMEWORK PRACTICE/DateParser.py
+++ HOMEWORK PRACTICE/DateParser.py
@@ -2,13 +2,19 @@
 class Date:
     days_in_month = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 
-    def __init__(self, date: str):
+    def __innit__(self, date: str, other, end_date: str):
         date_parts = date.split(".")
+        second_date_parts = end_date.split(".")
         if len(date_parts) != 3:
             raise ValueError(f"invalid date {date}. must be in format day.month.year")
+        elif len(second_date_parts) != 3:
+            raise ValueError(f"invalid date {end_date}. must be in format day.month.year")
         self.day = int(date_parts[0])
         self.month = int(date_parts[1])
         self.year = int(date_parts[2])
+        other.day = int(second_date_parts[0])
+        other.month = int(second_date_parts[1])
+        other.year = int(second_date_parts[2])
 
     def __gt__(self, other):
         if self.year > other.year:
@@ -44,3 +50,27 @@ class Date:
         if self.year < 1:
             return False
         return True
+
+
+    def count_days_between_dates(self, other):
+        days_passed = (other.year - self.year) * 365
+        if self.month > other.month:
+            full_months_passed = 0
+            for i in range(self.month - 1, other.month - 1, -1):
+                full_months_passed += self.days_in_month[i]
+            days_passed -= (full_months_passed + other.day - self.day)
+
+        if self.month < other.month:
+            full_months_passed = 0
+            for i in range(self.month + 1, other.month + 1):
+                full_months_passed += self.days_in_month[i]
+            days_passed += (full_months_passed - self.day + other.day)
+        else:
+            days_passed += self.day - other.day
+
+        return days_passed
+
+    def leap_year(self):
+        pass
+
+
---
+++ HOMEWORK PRACTICE/days_between_dates.py
@@ -0,0 +1,11 @@
+from DateParser import *
+def main():
+    start_date = str(input("enter your start date: "))
+    end_date = str(input("enter your end date: "))
+
+    print(Date(start_date,end_date))
+
+
+
+
+main()
